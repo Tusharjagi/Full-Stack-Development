620bea0fca46b1d94a0c05674b1e71d460023752
Tusharjagi <tusharjagi@gmail.com>
2022-03-14 14:01:35 +0530

    Data Binding

--- Angular/src/app/menu/menu.component.ts
+++ Angular/src/app/menu/menu.component.ts
@@ -1,50 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Dish } from '../Shared/dish';
 
-const DISHES: Dish [] =
-[
-  {
-    id: '0',
-    name: 'Uthappizza',
-    image: '/assets/images/uthappizza.png',
-    category: 'mains',
-    featured: true,
-    label: 'Hot',
-    price: '4.99',
-    // tslint:disable-next-line:max-line-length
-    description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'
-  },
-  {
-    id: '1',
-    name: 'Zucchipakoda',
-    image: '/assets/images/zucchipakoda.png',
-    category: 'appetizer',
-    featured: false,
-    label: '',
-    price: '1.99',
-    description: 'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'
-  },
-  {
-    id: '2',
-    name: 'Vadonut',
-    image: '/assets/images/vadonut.png',
-    category: 'appetizer',
-    featured: false,
-    label: 'New',
-    price: '1.99',
-    description: 'A quintessential ConFusion experience, is it a vada or is it a donut?'
-  },
-  {
-    id: '3',
-    name: 'ElaiCheese Cake',
-    image: '/assets/images/elaicheesecake.png',
-    category: 'dessert',
-    featured: false,
-    label: '',
-    price: '2.99',
-    description: 'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'
-  }
- ];
+import { DISHES } from '../Shared/dishes';
+
 
 
 @Component({
@@ -53,13 +11,18 @@ const DISHES: Dish [] =
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent implements OnInit {
-  dishes =  DISHES;
 
-  selectedDish: Dish = DISHES[0];
+  dishes: Dish[] = DISHES;
+
+  selectedDish!: Dish ;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelect(dish:Dish){
+    this.selectedDish = dish;
   }
 
 }
