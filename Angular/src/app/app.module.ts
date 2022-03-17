e6a2a58217312fc8036955c67b27ed072f27e4bc
Tusharjagi <tusharjagi@gmail.com>
2022-03-17 14:22:18 +0530

    Header and Footer

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -7,18 +7,24 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import {MatButtonModule} from '@angular/material/button';
+// import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 import 'hammerjs';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 import { DishService } from './services/dish.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
@@ -26,7 +32,9 @@ import { DishService } from './services/dish.service';
     FlexLayoutModule,
     MatListModule,
     MatGridListModule,
-    MatCardModule
+    MatCardModule,
+    MatButtonModule,
+    // AngularFontAwesomeModule
   ],
   providers: [DishService],
   bootstrap: [AppComponent]
