3638aeb825fe9b865a2828f205b4e447b0d21fd8
Tusharjagi <tusharjagi@gmail.com>
2022-03-17 17:37:25 +0530

    Routing

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -6,16 +6,21 @@ import { MatListModule } from '@angular/material/list';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { AppComponent } from './app.component';
-import { MenuComponent } from './menu/menu.component';
+import { DishService } from './services/dish.service';
 import {MatButtonModule} from '@angular/material/button';
 // import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 import 'hammerjs';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
-
-import { DishService } from './services/dish.service';
+import { MenuComponent } from './menu/menu.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+import { AppRoutingModule } from './app-routing/app-routing.module';
+
 
 
 @NgModule({
@@ -24,7 +29,10 @@ import { FooterComponent } from './footer/footer.component';
     MenuComponent,
     DishdetailComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
@@ -34,6 +42,7 @@ import { FooterComponent } from './footer/footer.component';
     MatGridListModule,
     MatCardModule,
     MatButtonModule,
+    AppRoutingModule
     // AngularFontAwesomeModule
   ],
   providers: [DishService],
