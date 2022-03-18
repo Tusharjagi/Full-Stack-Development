2197607236993c2d66c359547fc4ccf1c4d7e35c
Tusharjagi <tusharjagi@gmail.com>
2022-03-18 20:10:04 +0530

    SPA Routing Parameter

--- Angular/src/app/app-routing/routes.ts
+++ Angular/src/app/app-routing/routes.ts
@@ -9,6 +9,7 @@ import { ContactComponent } from '../contact/contact.component';
 export const routes: Routes = [
     {path: 'home', component: HomeComponent},
     {path: 'menu',component: MenuComponent},
+    {path: 'dishdetail/:id', component: DishdetailComponent},
     {path: 'contact',component: ContactComponent},
     {path: 'about',component: AboutComponent},
     {path: '',redirectTo: '/home',pathMatch:'full'}
--- Angular/src/app/dishdetail/dishdetail.component.ts
+++ Angular/src/app/dishdetail/dishdetail.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { Params,ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 import { Dish } from '../Shared/dish';
+import { DishService } from '../services/dish.service';
 
 
 @Component({
@@ -9,13 +12,22 @@ import { Dish } from '../Shared/dish';
 })
 export class DishdetailComponent implements OnInit {
 
-  @Input()
   dish!: Dish;
  
 
-  constructor() { }
+  constructor(private dishService: DishService,
+    private route: ActivatedRoute,
+    private location: Location) { }
 
   ngOnInit(): void {
+    let id = this.route.snapshot.params['id'];
+    this.dish = this.dishService.getDish(id);
+
+
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
