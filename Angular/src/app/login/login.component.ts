79d43d0e056988262644dc76a40dabe4ba72a8c1
Tusharjagi <tusharjagi@gmail.com>
2022-03-21 01:02:32 +0530

    Template Driven part two

--- Angular/src/app/login/login.component.ts
+++ Angular/src/app/login/login.component.ts
@@ -1,17 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog , MatDialogRef} from '@angular/material/dialog';
 
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  user = {username: '', password: '', remember: false};
+  user = {username: '',
+          password: '',
+          remember: false};
 
   constructor(public dialogRef: MatDialogRef<LoginComponent>) { }
 
   ngOnInit(): void {
+    
   }
 
   onSubmit(){
