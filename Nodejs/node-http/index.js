fb1e984fa40b8ffed5375b30a0cbb90c65229183
Tusharjagi <tusharjagi@gmail.com>
2022-04-08 13:42:46 +0530

    Node and HTTP Module

--- Nodejs/node-http/index.js
+++ Nodejs/node-http/index.js
@@ -1,14 +1,57 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const server = http.createServer((req,res) => {
-    console.log(req.headers);
+    console.log("Request for "+req.url + 'byt method ' +req.method);
 
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<html><body><h1>Hello,world !!!!!!!!</h1></body</html>')
+    if(req.method == 'GET'){
+        var fileUrl;
+        if(req.url == '/') 
+        {
+            fileUrl = '/index.html';
+        }
+        else 
+        {
+            fileUrl = req.url;
+        }
+        var filePath = path.resolve('./public'+fileUrl);
+        const fileExt = path.extname(filePath);
+        if(fileExt == '.html')
+        {
+            fs.exists(filePath,(exists) => { 
+                if(!exists)
+                {
+                    res.statusCode = 404;
+                    res.setHeader('Content-Type','text/html');
+                    res.end('<html><body><h1> Error 404:'+ fileUrl + ' not found <h1></body></html>');
+
+                    return;
+                }
+                res.statusCode = 200;
+                res.setHeader('Content-Type','text/html');
+                fs.createReadStream(filePath).pipe(res)
+             })
+        }
+        else {
+            res.statusCode = 404;
+            res.setHeader('Content-Type','text/html');
+            res.end('<html><body><h1> Error 404:'+ fileUrl + ' not an HTML file <h1></body></html>');
+
+            return;
+        }
+    }
+    else{
+        res.statusCode = 404;
+        res.setHeader('Content-Type','text/html');
+        res.end('<html><body><h1> Error 404:'+ req.method + ' not supported <h1></body></html>');
+
+        return;
+
+    }
 
 })
 
