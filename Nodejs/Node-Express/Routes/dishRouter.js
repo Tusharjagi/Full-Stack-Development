83568c4b5915ed52652d5c554c3a24100b28a917
Tusharjagi <tusharjagi@gmail.com>
2022-04-09 00:21:20 +0530

    DishId route update

--- Nodejs/Node-Express/Routes/dishRouter.js
+++ Nodejs/Node-Express/Routes/dishRouter.js
@@ -26,25 +26,28 @@ dishRouter.route('/')
     res.end('Deleting all the dishes !!! ');
 });
 
+dishRouter.route('/:dishId')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req, res, next) => {
+    res.end('Will send details of the dish :'
+    + req.params.dishId + ' to you !!! ');
+})
+.post((req,res,next) => {
+    res.statusCode = 403;
+    res.end('POST Operation not supported on /dishes/'
+            + req.params.dishId);
+})
+.put((req,res,next) => {
+    res.write('Updating the dish: ' + req.params.dishId + '\n');
+    res.end('Will update the dish: ' + req.body.name + 
+            ' With details: ' + req.body.description);
+})
+.delete((req, res, next) => {
+    res.end('Deleting dish: ' + req.params.dishId);
+});
+
 module.exports = dishRouter;
-
-// app.get('/dishes/:dishId', (req, res, next) => {
-//     res.end('Will send details of the dish :'
-//     + req.params.dishId + ' to you !!! ');
-// });
-
-// app.post('/dishes/:dishId', (req,res,next) => {
-//     res.statusCode = 403;
-//     res.end('POST Operation not supported on /dishes/'
-//             + req.params.dishId);
-// });
-
-// app.put('/dishes/:dishId', (req,res,next) => {
-//     res.write('Updating the dish: ' + req.params.dishId + '\n');
-//     res.end('Will update the dish: ' + req.body.name + 
-//             ' With details: ' + req.body.description);
-// });
-
-// app.delete('/dishes/:dishId', (req, res, next) => {
-//     res.end('Deleting dish: ' + req.params.dishId);
-// });
