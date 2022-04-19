32a4e03d3bb7cffe9150e5ce4ebd3412c571b7ad
Tusharjagi <tusharjagi@gmail.com>
2022-04-19 15:45:31 +0530

    Express-Session part 1

--- Nodejs/ConfusionServer/app.js
+++ Nodejs/ConfusionServer/app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('./node_modules/express-session');
+var FileStore = require('session-file-store')(session);
 
 
 var indexRouter = require('./routes/index');
@@ -33,12 +35,20 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(cookieParser('12345-67890-09876-54321'));
+// app.use(cookieParser('12345-67890-09876-54321'));
+
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
 
 function auth(req,res,next){
-  console.log(req.signedCookies);
+  console.log(req.session);
 
-  if(!req.signedCookies.user)
+  if(!req.session.user)
   {
     var authHeader = req.headers.authorization;
 
@@ -58,7 +68,7 @@ function auth(req,res,next){
 
     if(username === 'admin' && password === 'password')
     {
-      res.cookie('user', 'admin', {signed: true})
+      req.session.user = 'admin';
       next();
     }
     else{
@@ -70,7 +80,7 @@ function auth(req,res,next){
     }
   }
   else{
-    if(req.signedCookies.user === 'admin'){
+    if(req.session.user === 'admin'){
       next();
     }
     else {
