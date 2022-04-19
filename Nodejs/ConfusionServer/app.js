cc289d8e4a42572b60c5e9f2950baeec1ce55424
Tusharjagi <tusharjagi@gmail.com>
2022-04-19 14:32:34 +0530

    Basic authentication

--- Nodejs/ConfusionServer/app.js
+++ Nodejs/ConfusionServer/app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
@@ -32,6 +33,40 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+function auth(req,res,next){
+  console.log(req.headers);
+
+  var authHeader = req.headers.authorization;
+  if(!authHeader)
+  {
+    var err= new Error('You are not authenticated !');
+
+    res.setHeader('WWW-Authenticate','Basic');
+    err.status = 401;
+    return next(err);
+
+  }
+  var auth = new Buffer(authHeader.split(' ')[1], 'base64' ).toString().split(':');
+
+  var username = auth[0];
+  var password = auth[1];
+
+  if(username === 'admin' && password === 'password')
+  {
+    next();
+  }
+  else{
+    var err= new Error('You are not authenticated !');
+
+    res.setHeader('WWW-Authenticate','Basic');
+    err.status = 401;
+    return next(err);
+  }
+}
+
+app.use(auth);
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
