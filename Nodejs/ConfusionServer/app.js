d439f2a3299d42b23ba2c60f07b0ba705cb2888b
Tusharjagi <tusharjagi@gmail.com>
2022-04-19 18:51:04 +0530

    passport

--- Nodejs/ConfusionServer/app.js
+++ Nodejs/ConfusionServer/app.js
@@ -6,7 +6,8 @@ const bodyParser = require('body-parser');
 var logger = require('morgan');
 var session = require('./node_modules/express-session');
 var FileStore = require('session-file-store')(session);
-
+var passport = require('passport');
+var authenticate = require('./authenticate');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -46,29 +47,23 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 function auth(req,res,next){
-  console.log(req.session);
 
-  if(!req.session.user){
+  if(!req.user){
     
       var err= new Error('You are not authenticated !');
-
       res.setHeader('WWW-Authenticate','Basic');
       err.status = 401;
       return next(err);
   }
   else{
-    if(req.session.user === 'authenticated'){
-      next();
-    }
-    else {
-      var err= new Error('You are not authenticated !');
-      err.status = 403;
-      return next(err);
-    }
+    next();
   }
 
 }
--- Nodejs/ConfusionServer/models/user.js
+++ Nodejs/ConfusionServer/models/user.js
@@ -1,21 +1,15 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var passportLocalMongoose = require('passport-local-mongoose');
 
 var User = new Schema({
-    username: {
-        type: String,
-        require: true,
-        unique: true
-    },
-    password:{
-        type: String,
-        required: true
-    },
     admin: {
         type: Boolean,
         default: false
     }
 
 });
+
+User.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model('User', User);
