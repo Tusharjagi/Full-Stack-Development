276312a93618650bd78ba3ab168254b20d4d3bfd
Tusharjagi <tusharjagi@gmail.com>
2022-04-25 23:56:17 +0530

    File Upload

--- Nodejs/ConfusionServer/app.js
+++ Nodejs/ConfusionServer/app.js
@@ -15,6 +15,7 @@ var users = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
+var uploadRouter = require('./routes/uploadRouter');
 
 const mongoose = require('mongoose');
 
@@ -84,6 +85,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
+app.use('/imageUpload', uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
