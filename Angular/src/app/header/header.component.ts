9315142a21fe3ad2ee0a9212b990acc70ab597b9
tusharjagi <tusharjagi@gmail.com>
2022-05-05 13:08:53 +0530

    Header

--- Angular/src/app/header/header.component.ts
+++ Angular/src/app/header/header.component.ts
@@ -1,21 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatDialog, MatDialogRef} from '@angular/material/dialog';
+import { Subscription } from 'rxjs';
 import { LoginComponent } from '../login/login.component';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
 
-  constructor(private dialog: MatDialog) { }
+  username!: string ;
+  subscription!: Subscription;
+
+  constructor(private dialog: MatDialog,
+    private authService : AuthService) { }
 
   ngOnInit(): void {
+    this.authService.loadUserCredentials();
+    this.subscription = this.authService.getUsername()
+        .subscribe(name=> { console.log(name); this.username = name; });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
   openLoginForm(){
-    this.dialog.open(LoginComponent, {width:'500px', height:'450px'});
+    const loginRef = this.dialog.open(LoginComponent, {width:'500px', height:'450px'});
+
+    loginRef.afterClosed()
+      .subscribe(result => {
+        console.log(result);
+      });
+  }
+
+  logOut(){
+    this.username ;
+    this.authService.logOut();
   }
 
 }
