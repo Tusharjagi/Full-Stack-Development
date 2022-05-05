e40f5abb1eb918f33c47f3be3e81c9d8db85cf29
tusharjagi <tusharjagi@gmail.com>
2022-05-05 18:35:45 +0530

    Revert "Header"
    This reverts commit 9315142a21fe3ad2ee0a9212b990acc70ab597b9.

--- Angular/src/app/header/header.component.ts
+++ Angular/src/app/header/header.component.ts
@@ -1,44 +1,21 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialog, MatDialogRef} from '@angular/material/dialog';
-import { Subscription } from 'rxjs';
 import { LoginComponent } from '../login/login.component';
-import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit, OnDestroy {
+export class HeaderComponent implements OnInit {
 
-  username!: string ;
-  subscription!: Subscription;
-
-  constructor(private dialog: MatDialog,
-    private authService : AuthService) { }
+  constructor(private dialog: MatDialog) { }
 
   ngOnInit(): void {
-    this.authService.loadUserCredentials();
-    this.subscription = this.authService.getUsername()
-        .subscribe(name=> { console.log(name); this.username = name; });
-  }
-
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
   }
 
   openLoginForm(){
-    const loginRef = this.dialog.open(LoginComponent, {width:'500px', height:'450px'});
-
-    loginRef.afterClosed()
-      .subscribe(result => {
-        console.log(result);
-      });
-  }
-
-  logOut(){
-    this.username ;
-    this.authService.logOut();
+    this.dialog.open(LoginComponent, {width:'500px', height:'450px'});
   }
 
 }
