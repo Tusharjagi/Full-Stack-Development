8cfc7a27174b626e7656626b3a1fe443a131a8aa
Tusharjagi <tusharjagi@gmail.com>
2023-01-04 12:37:39 +0530

    module caching

--- Node-Js/index.js
+++ Node-Js/index.js
@@ -1,2 +1,9 @@
-require("./batman");
-require("./superman");
+const SuperHero = require("./super-hero");
+
+const batman = new SuperHero("Batman");
+console.log(batman.getName());
+batman.setName("Bruce wills");
+console.log(batman.getName());
+
+const superman = new SuperHero("Superman");
+console.log(superman.getName());
---
+++ Node-Js/super-hero.js
@@ -0,0 +1,15 @@
+class SuperHero {
+  constructor(name) {
+    this.name = name;
+  }
+
+  getName() {
+    return this.name;
+  }
+
+  setName(name) {
+    this.name = name;
+  }
+}
+
+module.exports = SuperHero;
