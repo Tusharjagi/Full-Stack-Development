745a81d08013e44a1b42809efff12482b78eee04
Tusharjagi <tusharjagi@gmail.com>
2023-01-04 11:26:46 +0530

    module wrapper

--- Node-Js/iife.js
+++ Node-Js/iife.js
@@ -1,7 +1,19 @@
-(function () {
+(function (message) {
   const superHero = "Batman";
-  console.log(superHero);
-})()(function () {
+  console.log(message, superHero);
+})("Hello");
+
+(function (message) {
   const superHero = "Superman";
-  console.log(superHero);
-})();
+  console.log(message, superHero);
+})("Hey");
+
+/*
+  module wrapper
+  
+  (function(exports, require, module, __filename, __dirname) {
+    const superHero = "Batman";
+    console.log(superHero);
+  })
+
+*/
