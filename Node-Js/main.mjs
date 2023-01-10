0ee03ce2eec542795ae0d359cb345d6036168a62
Tusharjagi <tusharjagi@gmail.com>
2023-01-10 11:23:53 +0530

    module export in ES5

---
+++ Node-Js/main.mjs
@@ -0,0 +1,6 @@
+import math from "./math-esm.mjs";
+
+const { add, subtract } = math;
+
+console.log(add(5, 5));
+console.log(subtract(5, 5));
---
+++ Node-Js/math-esm.mjs
@@ -0,0 +1,14 @@
+const add = (a, b) => {
+  return a + b;
+};
+
+const subtract = (a, b) => {
+  return a - b;
+};
+
+export default {
+  add,
+  subtract,
+};
+
+// export default add;
