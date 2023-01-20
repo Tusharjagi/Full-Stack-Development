a09846592d497ec9b3ae4cb8f19a2cca4a0da4f6
Tusharjagi <tusharjagi@gmail.com>
2023-01-20 15:54:49 +0530

    path builtin module

--- Node-Js/index.js
+++ Node-Js/index.js
@@ -1,4 +1,28 @@
-const path = require("node:path");
+const path = require("path");
 
-console.log(__filename);
-console.log(__dirname);
+// console.log(__filename);
+// console.log(__dirname);
+
+// console.log(path.basename(__filename));
+// console.log(path.basename(__dirname));
+
+// console.log(path.extname(__filename));
+// console.log(path.extname(__dirname));
+
+// console.log(path.parse(__filename));
+// console.log(path.format(path.parse(__filename)));
+
+// console.log(path.isAbsolute(__filename));
+// console.log(path.isAbsolute("./data.json"));
+
+// console.log(path.join("folder1", "folder2", "index.html"));
+// console.log(path.join("/folder1", "folder2", "index.html"));
+// console.log(path.join("/folder1", "//folder2", "index.html"));
+// console.log(path.join("/folder1", "//folder2", "../index.html"));
+// console.log(path.join(__dirname, "data.json"));
+
+console.log(path.resolve("folder1", "folder2", "index.html"));
+console.log(path.resolve("/folder1", "folder2", "index.html"));
+console.log(path.resolve("/folder1", "//folder2", "index.html"));
+console.log(path.resolve("/folder1", "//folder2", "../index.html"));
+console.log(path.resolve(__dirname, "data.json"));
